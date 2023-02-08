99fdd5a59f6f6de28c4c1c2fa7ef3004dc905ff3
theotsori <theodoretsori@gmail.com>
2023-02-08 09:37:04 +0300

    filestorage hbnb

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -2,12 +2,5 @@
 
 from models.engine.file_storage import FileStorage
 
-storage = None
-
-def create_storage():
-    global storage
-    if storage is None:
-        storage = FileStorage()
-        storage.reload()
-
-create_storage()
+storage = FileStorage
+storage.reload
