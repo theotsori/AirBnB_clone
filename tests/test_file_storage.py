51e4aa7fc6e1cd6c46f7806d67afcbac48e8b710
theotsori <theodoretsori@gmail.com>
2023-02-11 13:42:05 +0300

    HBnB console update x1

---
+++ tests/test_file_storage.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import os
+import unittest
+import json
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+
+    def setUp(self):
+        """Setting up the test environment"""
+        self.storage = FileStorage("file.json")
+
+    def test_all(self):
+        """Test if the 'all' method returns the right object"""
+        state = State()
+        state.name = "California"
+        self.storage.new(state)
+        self.assertIn(state, self.storage.all().values())
+
+    def test_new(self):
+        """Test if the 'new' method works correctly"""
+        state = State()
+        state.name = "California"
+        self.storage.new(state)
+        all_objects = self.storage.all()
+        key = "{}.{}".format(state.__class__.__name__, state.id)
+        self.assertIn(key, all_objects.keys())
+
+    def test_save_reload(self):
+        """Test if the 'save' and 'reload' methods work correctly"""
+        state = State()
+        state.name = "California"
+        self.storage.new(state)
+        self.storage.save()
+        self.storage.reload()
+        all_objects = self.storage.all()
+        key = "{}.{}".format(state.__class__.__name__, state.id)
+        self.assertIn(key, all_objects.keys())
+
+    def tearDown(self):
+        """Cleaning up after the test"""
+        try:
+            os.remove("file.json")
+        except:
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_init.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+class TestFileStorage(unittest.TestCase):
+
+    def test_new(self):
+        storage = FileStorage("file.json")
+        state = State()
+        state.name = "California"
+        storage.new(state)
+        self.assertIn(state, storage.all().values())
+
+    def test_reload(self):
+        storage = FileStorage("file.json")
+        state = State()
+        state.name = "California"
+        storage.new(state)
+        storage.save()
+        storage.reload()
+        #  self.assertIn(state, storage.all().values())
+
+    def test_save(self):
+        storage = FileStorage("file.json")
+        state = State()
+        state.name = "California"
+        storage.new(state)
+        storage.save()
+        storage_all = storage.all()
+        state_key = state.__class__.__name__ + "." + state.id
+        self.assertIn(state_key, storage_all.keys())
+
+if __name__ == '__main__':
+    unittest.main()
